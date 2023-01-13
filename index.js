009f2ea1a1fe759a7010872ea5e8cba33294c326
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-01-13 23:19:01 +0600

    Create New and category data loading API1

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors());
 
 // consume data
 const categories = require('./data/categories.json');
+const news = require('./data/news.json')
 
 app.get('/', (req, res) => {
     res.send('News API running');
@@ -15,6 +16,26 @@ app.get('/', (req, res) => {
 // route anujayi data sent krr jnno
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+});
+
+// category wise khula, jehe2 category wise onk news thakbe tai filter kora hoise
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+
+    // jehe2 08 category te kono data nei tai 08 mane sob news data
+    if (id === '08') {
+        res.send(news);
+    } else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+});
+
+// id diye news khuja
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id; // req.params means full object, params.id means sei id tai
+    const selectedNews = news.find(n => n._id === id); //n means news, News.json theke find jotogulo News.json er vitor _id wala element ase
+    res.send(selectedNews);
 });
 
 
